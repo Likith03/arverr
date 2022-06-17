d378dac8159c7cc812f93916bf8d0446fa06452c
Likith03 <106599406+Likith03@users.noreply.github.com>
2022-06-17 10:19:34 +0530

    Add files via upload

---
+++ Day13-TS/script/one.js
@@ -0,0 +1,16 @@
+"use strict";
+let message;
+/*
+let value:number = 555;
+let value:boolean = true/false;
+let value:string = "welcome to your life";
+let value:object = Object.create(null);
+let value:string[] = ["hello", "there"];
+let value:array<number> = [5, 6];
+
+let value:undefined;
+let value:null;
+let value:any;*/
+message = "hello there";
+message = 555;
+alert(message);
---
+++ Day13-TS/src/one.ts
@@ -0,0 +1,17 @@
+let message:(string | number);
+
+/*
+let value:number = 555;
+let value:boolean = true/false;
+let value:string = "welcome to your life";
+let value:object = Object.create(null);
+let value:string[] = ["hello", "there"];
+let value:array<number> = [5, 6];
+
+let value:undefined;
+let value:null;
+let value:any;*/
+
+message = "hello there";
+message = 555;
+alert(message);
